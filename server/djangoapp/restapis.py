51f4663a121781c3f84887f7132236fea9dd364b
sahnawaz <sahnawazhussain852@gmail.com>
2025-12-07 02:58:58 -0500

    module 4 lab done

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,5 +1,5 @@
 # Uncomment the imports below before you add the function code
-# import requests
+import requests
 import os
 from dotenv import load_dotenv
 
@@ -45,4 +45,11 @@ def analyze_review_sentiments(text):
 
 # def post_review(data_dict):
 # Add code for posting review
-
+def post_review(data_dict):
+    request_url = backend_url+"/insert_review"
+    try:
+        response = requests.post(request_url,json=data_dict)
+        print(response.json())
+        return response.json()
+    except:
+        print("Network exception occurred")
